b2141f5193852cfcd5fe374a57dd2054ae5efd38
Andrey Kislyuk <kislyuk@gmail.com>
2021-03-12 05:59:17 +0000

    WIP

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import itertools
 import os.path
 from setuptools import setup, find_packages, Extension
 
-MARISA_ROOT_DIR = "taxoniq/vendored/marisa-trie/marisa-trie"
+MARISA_ROOT_DIR = "marisa-trie/marisa-trie"
 MARISA_SOURCE_DIR = os.path.join(MARISA_ROOT_DIR, "lib")
 MARISA_INCLUDE_DIR = os.path.join(MARISA_ROOT_DIR, "include")
 MARISA_FILES = [
@@ -22,7 +22,6 @@ MARISA_FILES = [
     "marisa/grimoire/trie/*.cc",
     "marisa/grimoire/vector/*.cc",
 ]
-
 MARISA_FILES[:] = itertools.chain(
     *(glob.glob(os.path.join(MARISA_SOURCE_DIR, path))
       for path in MARISA_FILES))
@@ -68,16 +67,16 @@ setup(
         "include_dirs": [MARISA_SOURCE_DIR, MARISA_INCLUDE_DIR]
     })],
     ext_modules=[
-        Extension("marisa_trie", [
-            "taxoniq/vendored/marisa-trie/src/agent.cpp",
-            "taxoniq/vendored/marisa-trie/src/base.cpp",
-            "taxoniq/vendored/marisa-trie/src/iostream.cpp",
-            "taxoniq/vendored/marisa-trie/src/key.cpp",
-            "taxoniq/vendored/marisa-trie/src/keyset.cpp",
-            "taxoniq/vendored/marisa-trie/src/marisa_trie.cpp",
-            "taxoniq/vendored/marisa-trie/src/query.cpp",
-            "taxoniq/vendored/marisa-trie/src/std_iostream.cpp",
-            "taxoniq/vendored/marisa-trie/src/trie.cpp"
+        Extension("taxoniq.vendored.marisa_trie", [
+            "marisa-trie/src/agent.cpp",
+            "marisa-trie/src/base.cpp",
+            "marisa-trie/src/iostream.cpp",
+            "marisa-trie/src/key.cpp",
+            "marisa-trie/src/keyset.cpp",
+            "marisa-trie/src/marisa_trie.cpp",
+            "marisa-trie/src/query.cpp",
+            "marisa-trie/src/std_iostream.cpp",
+            "marisa-trie/src/trie.cpp"
         ], include_dirs=[MARISA_INCLUDE_DIR])
     ],
     classifiers=[
