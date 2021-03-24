65176cacbf64aedabefb3bb674e8a1cc7871eaba
Andrey Kislyuk <kislyuk@gmail.com>
2021-03-24 18:43:48 +0000

    Fixes for release script

--- common.mk
+++ common.mk
@@ -12,7 +12,7 @@ release-patch:
 	$(eval export TAG=$(shell git describe --tags --match 'v*.*.*' | perl -ne '/^v(\d+)\.(\d+)\.(\d+)/; print "v$$1.$$2.@{[$$3+1]}"'))
 	$(MAKE) release
 
-init-release-env:
+release:
 	@if ! git diff --cached --exit-code; then echo "Commit staged files before proceeding"; exit 1; fi
 	@if ! type -P pandoc; then echo "Please install pandoc"; exit 1; fi
 	@if ! type -P sponge; then echo "Please install moreutils"; exit 1; fi
@@ -25,9 +25,6 @@ init-release-env:
 	$(eval RELEASES_API=https://api.github.com/repos/${REMOTE}/releases)
 	$(eval UPLOADS_API=https://uploads.github.com/repos/${REMOTE}/releases)
 	git pull
-
-release:
-	$(MAKE) init-release-env
 	@if [[ -z $$TAG ]]; then echo "Use release-{major,minor,patch}"; exit 1; fi
 	git clean -x --force $$(python setup.py --name)
 	sed -i -e "s/version=\([\'\"]\)[0-9]*\.[0-9]*\.[0-9]*/version=\1$${TAG:1}/" setup.py
@@ -39,7 +36,7 @@ release:
 	    if [[ -f Changes.md ]]; then cat $$TAG_MSG <(echo) Changes.md | sponge Changes.md; git add Changes.md; fi; \
 	    if [[ -f Changes.rst ]]; then cat <(pandoc --from markdown --to rst $$TAG_MSG) <(echo) Changes.rst | sponge Changes.rst; git add Changes.rst; fi; \
 	    git commit -m ${TAG}; \
-	    git tag --sign --annotate --file $$TAG_MSG ${TAG}
+	    git tag --annotate --file $$TAG_MSG ${TAG}
 	git push --follow-tags
 	http --check-status --auth ${GH_AUTH} ${RELEASES_API} tag_name=${TAG} name=${TAG} \
 	    body="$$(git tag --list ${TAG} -n99 | perl -pe 's/^\S+\s*// if $$. == 1' | sed 's/^\s\s\s\s//')"
@@ -52,17 +49,28 @@ release:
 #	$(MAKE) release-docs
 
 release-db-packages:
-	$(MAKE) init-release-env
+	@if ! git diff --cached --exit-code; then echo "Commit staged files before proceeding"; exit 1; fi
+	@if ! type -P pandoc; then echo "Please install pandoc"; exit 1; fi
+	@if ! type -P sponge; then echo "Please install moreutils"; exit 1; fi
+	@if ! type -P http; then echo "Please install httpie"; exit 1; fi
+	@if ! type -P twine; then echo "Please install twine"; exit 1; fi
+	$(eval REMOTE=$(shell git remote get-url origin | perl -ne '/([^\/\:]+\/.+?)(\.git)?$$/; print $$1'))
+	$(eval GIT_USER=$(shell git config --get user.email))
+	$(eval GH_AUTH=$(shell if grep -q '@github.com' ~/.git-credentials; then echo $$(grep '@github.com' ~/.git-credentials | python3 -c 'import sys, urllib.parse as p; print(p.urlparse(sys.stdin.read()).netloc.split("@")[0])'); else echo $(GIT_USER); fi))
+	$(eval REPOS_API=https://api.github.com/repos/${REMOTE})
+	$(eval RELEASES_API=https://api.github.com/repos/${REMOTE}/releases)
+	$(eval UPLOADS_API=https://uploads.github.com/repos/${REMOTE}/releases)
+	git pull
 	sed -i -e "s/20[0-9][0-9].[0-9]*.[0-9]*/$$(cat latest-dir | cut -f 1-3 -d - | sed -e 's/-/./g' -e 's/\.0/\./')/" setup.py db_packages/*/setup.py
 	git add setup.py db_packages/*/setup.py
 	git commit -m "Update data packages to version $$(cat latest-dir | cut -f 1-3 -d -)"
 	git push
 	for p in db_packages/*; do (cd $$p; python setup.py bdist_wheel); done
-	twine upload db_packages/*/dist/*.whl --sign --verbose
+	twine upload db_packages/*/dist/*.whl --verbose
 
 release-pypi:
 	python setup.py sdist
-	twine upload dist/*.tar.gz dist/*.whl --sign --verbose
+	twine upload dist/*.tar.gz dist/*.whl --verbose
 
 release-docs:
 	$(MAKE) docs
