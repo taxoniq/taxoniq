74bafa9a28298d7d23e53aa227dbb5754ca68ec4
Andrey Kislyuk <kislyuk@gmail.com>
2021-05-12 16:18:05 -0700

    Release script: Correctly parse HTTPS origin URLs

--- release.mk
+++ release.mk
@@ -20,7 +20,7 @@ check-release-deps:
 	@if ! type -P twine; then echo "Please install twine"; exit 1; fi
 
 release: check-release-deps
-	$(eval REMOTE=$(shell git remote get-url origin | perl -ne '/([^\/\:]+\/.+?)(\.git)?$$/; print $$1'))
+	$(eval REMOTE=$(shell git remote get-url origin | perl -ne '/([^\/\:]+\/[^\/\:]+?)(\.git)?$$/; print $$1'))
 	$(eval GIT_USER=$(shell git config --get user.email))
 	$(eval GH_AUTH=$(shell if grep -q '@github.com' ~/.git-credentials; then echo $$(grep '@github.com' ~/.git-credentials | python3 -c 'import sys, urllib.parse as p; print(p.urlparse(sys.stdin.read()).netloc.split("@")[0])'); else echo $(GIT_USER); fi))
 	$(eval REPOS_API=https://api.github.com/repos/${REMOTE})
